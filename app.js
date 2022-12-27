94be09bdf70768efb688788bcd94cfca4c033a7e
MohamedAmineSafi04 <mohammedameenalsafi@gmail.com>
2022-12-27 21:26:43 +0100

    Handling PATCH Requests

--- app.js
+++ app.js
@@ -59,6 +59,24 @@ app.post('/api/v1/tours', (req, res) => {
   );
 });
 
+app.patch('/api/v1/tours/:id', (req, res) => {
+  const id = req.params.id * 1;
+  if (id > tours.length) {
+    return res.status(404).json({
+      //we user return to exit the function
+      status: 'fail',
+      message: 'invalid Id',
+    });
+  }
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      tour: '<Updated tour here...>',
+    },
+  });
+});
+
 const port = 8000;
 app.listen(port, () => {
   console.log(`App Running on port ${port}...`);
