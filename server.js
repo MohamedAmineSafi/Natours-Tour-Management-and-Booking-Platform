c4a6f04aa249081b6ed68f30e43f9198f943c7d9
Mohamed Amine Safi <mohammedameenalsafi@gmail.com>
2023-01-04 11:11:21 +0100

    Creating a Simple Tour Model

--- server.js
+++ server.js
@@ -17,6 +17,23 @@ mongoose
     console.log('DB Connection Successful');
   });
 
+const toursSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'A tour must have a name'],
+    unique: true,
+  },
+  rating: {
+    type: Number,
+    default: 4.5,
+  },
+  price: {
+    type: Number,
+    required: [true, 'A tour must have a price'],
+  },
+});
+const Tour = mongoose.model('Tour', toursSchema); // Uppercase for model name
+
 const app = require('./app');
 // console.log(process.env);
 
