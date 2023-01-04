91b55f4c753369965e6719b27ac5f896ceca2399
Mohamed Amine Safi <mohammedameenalsafi@gmail.com>
2023-01-04 11:02:49 +0100

    Connecting Our Database with the Express App

--- server.js
+++ server.js
@@ -1,5 +1,21 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 dotenv.config({ path: './config.env' }); // Before 'const app'
+
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
+  .then((connection) => {
+    // console.log(connection.connections);
+    console.log('DB Connection Successful');
+  });
 
 const app = require('./app');
 // console.log(process.env);
