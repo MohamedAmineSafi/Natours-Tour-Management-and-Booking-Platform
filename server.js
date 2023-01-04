66727c5f1cfe2565cd1330fb334267deceb8e135
Mohamed Amine Safi <mohammedameenalsafi@gmail.com>
2023-01-04 11:42:01 +0100

    Creating Documents and Testing the Model

--- server.js
+++ server.js
@@ -34,6 +34,21 @@ const toursSchema = new mongoose.Schema({
 });
 const Tour = mongoose.model('Tour', toursSchema); // Uppercase for model name
 
+const testTour = new Tour({
+  name: 'The Park Camper',
+  price: 997,
+});
+
+testTour
+  .save()
+  .then((doc) => {
+    console.log('***********');
+    console.log(doc);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
 const app = require('./app');
 // console.log(process.env);
 
