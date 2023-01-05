ab55698726206fccea4c35ea51adc63af80f34ed
Mohamed Amine Safi <mohammedameenalsafi@gmail.com>
2023-01-05 13:59:27 +0100

    Modelling the Tours

--- models/tourModel.js
+++ models/tourModel.js
@@ -5,15 +5,52 @@ const toursSchema = new mongoose.Schema({
     type: String,
     required: [true, 'A tour must have a name'],
     unique: true,
+    trim: true,
   },
-  rating: {
+  duration: {
+    type: Number,
+    required: [true, 'A tour must have a duration'],
+  },
+  maxGroupSize: {
+    type: Number,
+    required: [true, 'A tour must have a group size'],
+  },
+  difficulty: {
+    type: String,
+    required: [true, 'A tour must have a difficulty'],
+  },
+  ratingsAverage: {
     type: Number,
     default: 4.5,
+  },
+  ratingsQuantity: {
+    type: Number,
+    default: 0,
   },
   price: {
     type: Number,
     required: [true, 'A tour must have a price'],
   },
+  priceDiscount: Number,
+  summary: {
+    type: String,
+    required: [true, 'A tour must have a summary'],
+    trim: true, // remove all white space in the beginning and end of string
+  },
+  description: {
+    type: String,
+    trim: true,
+  },
+  imageCover: {
+    type: String,
+    required: [true, 'A tour must have a cover image'],
+  },
+  images: [String], //An Array of Strings
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+  },
+  startDates: [Date], //An Array of Dates
 });
 const Tour = mongoose.model('Tour', toursSchema); // Uppercase for model name
 
