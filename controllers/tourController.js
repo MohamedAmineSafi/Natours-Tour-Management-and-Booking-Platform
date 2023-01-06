a0aa8b03cc4d9127e62e9532341e065b51b1b11c
Mohamed Amine Safi <mohammedameenalsafi@gmail.com>
2023-01-06 16:31:53 +0100

    Making the API Better Filtering

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -6,7 +6,21 @@ const Tour = require('./../models/tourModel');
 
 exports.getAllTours = async (req, res) => {
   try {
-    const tours = await Tour.find();
+    const queryObj = { ...req.query };
+    const excludedFields = ['page', 'sort', 'limit', 'fields'];
+    excludedFields.forEach((el) => delete queryObj[el]);
+
+    // console.log(req.query); Gives access to url query like example.com/?key=value
+    const query = Tour.find(queryObj);
+
+    // CAN DO IT LIKE THIS TOO:
+    // const query = Tour.find()
+    //   .where('duration')
+    //   .equals(5)
+    //   .where('difficulty')
+    //   .equals('easy');
+
+    const tours = await query;
 
     res.status(200).json({
       status: 'success',
