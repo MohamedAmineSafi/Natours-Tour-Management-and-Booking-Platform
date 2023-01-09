62766ddc25ea29fe3916174ae3bb27e974bda098
Mohamed Amine Safi <mohammedameenalsafi@gmail.com>
2023-01-09 16:22:55 +0100

    Virtual Properties

--- models/tourModel.js
+++ models/tourModel.js
@@ -1,58 +1,70 @@
 const mongoose = require('mongoose');
 
-const toursSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: [true, 'A tour must have a name'],
-    unique: true,
-    trim: true,
+const toursSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, 'A tour must have a name'],
+      unique: true,
+      trim: true,
+    },
+    duration: {
+      type: Number,
+      required: [true, 'A tour must have a duration'],
+    },
+    maxGroupSize: {
+      type: Number,
+      required: [true, 'A tour must have a group size'],
+    },
+    difficulty: {
+      type: String,
+      required: [true, 'A tour must have a difficulty'],
+    },
+    ratingsAverage: {
+      type: Number,
+      default: 4.5,
+    },
+    ratingsQuantity: {
+      type: Number,
+      default: 0,
+    },
+    price: {
+      type: Number,
+      required: [true, 'A tour must have a price'],
+    },
+    priceDiscount: Number,
+    summary: {
+      type: String,
+      required: [true, 'A tour must have a summary'],
+      trim: true, // remove all white space in the beginning and end of string
+    },
+    description: {
+      type: String,
+      trim: true,
+    },
+    imageCover: {
+      type: String,
+      required: [true, 'A tour must have a cover image'],
+    },
+    images: [String], //An Array of Strings
+    createdAt: {
+      type: Date,
+      default: Date.now(),
+      select: false, // Hides 'createdAt'
+    },
+    startDates: [Date], //An Array of Dates
   },
-  duration: {
-    type: Number,
-    required: [true, 'A tour must have a duration'],
-  },
-  maxGroupSize: {
-    type: Number,
-    required: [true, 'A tour must have a group size'],
-  },
-  difficulty: {
-    type: String,
-    required: [true, 'A tour must have a difficulty'],
-  },
-  ratingsAverage: {
-    type: Number,
-    default: 4.5,
-  },
-  ratingsQuantity: {
-    type: Number,
-    default: 0,
-  },
-  price: {
-    type: Number,
-    required: [true, 'A tour must have a price'],
-  },
-  priceDiscount: Number,
-  summary: {
-    type: String,
-    required: [true, 'A tour must have a summary'],
-    trim: true, // remove all white space in the beginning and end of string
-  },
-  description: {
-    type: String,
-    trim: true,
-  },
-  imageCover: {
-    type: String,
-    required: [true, 'A tour must have a cover image'],
-  },
-  images: [String], //An Array of Strings
-  createdAt: {
-    type: Date,
-    default: Date.now(),
-    select: false, // Hides 'createdAt'
-  },
-  startDates: [Date], //An Array of Dates
+  {
+    // To Show the virtual props
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true },
+  }
+);
+
+toursSchema.virtual('durationWeeks').get(function () {
+  return this.duration / 7; // 'this' is the schema (current document) also we use regular function cause we need 'this'
 });
+
 const Tour = mongoose.model('Tour', toursSchema); // Uppercase for model name
 
 module.exports = Tour;
