1c784a2ada9d19dd01d49db44d1ea637cb60f5d6
Mohamed Amine Safi <mohammedameenalsafi@gmail.com>
2023-01-24 10:38:11 +0100

    Data Sanitization

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express');
 const morgan = require('morgan');
 const rateLimit = require('express-rate-limit');
 const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -26,6 +28,11 @@ const limiter = rateLimit({
 app.use('/api', limiter); // apply only to /api
 
 app.use(express.json({ limit: '10kb' })); // limit body to 10kb
+
+// Data Sanitization against query injection
+app.use(mongoSanitize());
+// Data Sanitization against XSS
+app.use(xss());
 
 app.use(express.static(`${__dirname}/public`)); // sets public as a root folder (localhost/overview.html)
 
