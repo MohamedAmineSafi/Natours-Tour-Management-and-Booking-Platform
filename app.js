3f0c39dc91b6e1d320dd6f30df630c796db44f4d
Mohamed Amine Safi <mohammedameenalsafi@gmail.com>
2023-01-25 19:50:19 +0100

    Creating and Getting Reviews

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
+const reviewRouter = require('./routes/reviewRoutes');
 const errorController = require('./controllers/errorController');
 
 const app = express();
@@ -58,6 +59,7 @@ app.use((req, res, next) => {
 // Routes
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
+app.use('/api/v1/reviews', reviewRouter);
 
 // It will only reach this point if the routes weren't called
 app.all('*', (req, res, next) => {
