038790d50a3fb8ae797d0452841d9c785aeab796
Mohamed Amine Safi <mohammedameenalsafi@gmail.com>
2023-02-02 11:09:21 +0100

    First Steps with Pug

--- app.js
+++ app.js
@@ -62,7 +62,10 @@ app.use((req, res, next) => {
 
 // Routes
 app.get('/', (req, res) => {
-  res.status(200).render('base');
+  res.status(200).render('base', {
+    tour: 'The Forest Hiker',
+    user: 'Amine',
+  });
 });
 
 app.use('/api/v1/tours', tourRouter);
