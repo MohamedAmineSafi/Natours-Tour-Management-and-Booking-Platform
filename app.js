1c9e04cdb339c95a7fe0e313b8f32d279373a09c
Mohamed Amine Safi <mohammedameenalsafi@gmail.com>
2023-02-06 21:13:12 +0100

    Logging in Users with Our API - Part 1

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const helmet = require('helmet');
 const mongoSanitize = require('express-mongo-sanitize');
 const xss = require('xss-clean');
 const hpp = require('hpp');
+const cookieParser = require('cookie-parser');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -35,6 +36,7 @@ const limiter = rateLimit({
 app.use('/api', limiter); // apply only to /api
 
 app.use(express.json({ limit: '10kb' })); // limit body to 10kb
+app.use(cookieParser());
 
 // Data Sanitization against query injection
 app.use(mongoSanitize());
