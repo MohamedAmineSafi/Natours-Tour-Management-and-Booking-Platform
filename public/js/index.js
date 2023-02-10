583d0c5a6bafa39d0a32a058a47000d71d70a625
Mohamed Amine Safi <mohammedameenalsafi@gmail.com>
2023-02-10 18:38:28 +0100

    Updating User Password with Our API

--- public/js/index.js
+++ public/js/index.js
@@ -1,10 +1,11 @@
 import '@babel/polyfill';
 import { login, logout } from './login';
-import { updateDate } from './updateSettings';
+import { updateSettings } from './updateSettings';
 
 const loginForm = document.querySelector('.form--login');
 const logOutBtn = document.querySelector('.nav__el--logout');
 const userDataForm = document.querySelector('.form-user-data');
+const userPasswordForm = document.querySelector('.form-user-password');
 
 if (loginForm) {
   loginForm.addEventListener('submit', (e) => {
@@ -25,6 +26,27 @@ if (userDataForm) {
     const name = document.getElementById('name').value;
     const email = document.getElementById('email').value;
 
-    updateDate(name, email);
+    updateSettings({ name, email }, 'data');
+  });
+}
+
+if (userPasswordForm) {
+  userPasswordForm.addEventListener('submit', async (e) => {
+    e.preventDefault();
+    document.querySelector('.btn--save-password').textContent = 'Updating...';
+
+    const passwordCurrent = document.getElementById('password-current').value;
+    const password = document.getElementById('password').value;
+    const passwordConfirm = document.getElementById('password-confirm').value;
+
+    await updateSettings(
+      { passwordCurrent, password, passwordConfirm },
+      'password'
+    ); // await to wait till the password change
+
+    document.querySelector('.btn--save-password').textContent = 'Save Password';
+    document.getElementById('password-current').value = '';
+    document.getElementById('password').value = '';
+    document.getElementById('password-confirm').value = '';
   });
 }
